9751ad9e647718edd2a18e86ed6010ced55c4267
LumberjacksIncorperated <robertcharlton23@outlook.com>
2018-12-04 12:33:46 +1100

    .

--- NautilusKeyboardClient/compileAndRun.sh
+++ NautilusKeyboardClient/compileAndRun.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 javac -cp ./java-websocket-1.3.9.jar *.java
-java -cp ./Java-WebSocket-1.3.9.jar:. NautilusKeyboardClient "ws://localhost:8887"
+java -cp ./Java-WebSocket-1.3.9.jar:. NautilusKeyboardClient "ws://45.77.234.39:8887"
